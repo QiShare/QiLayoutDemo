9ffc1846f353de3fed4efcc122fe72118e2960fd
huangxianshuai <huangxianshuai@360.cn>
2019-05-20 15:59:29 +0800

    init project

---
+++ QiLayoutDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  QiLayoutDemo
+//
+//  Created by huangxianshuai on 2019/5/20.
+//  Copyright © 2019年 QiShare. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
---
+++ QiLayoutDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QiLayoutDemo
+//
+//  Created by huangxianshuai on 2019/5/20.
+//  Copyright © 2019年 QiShare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
