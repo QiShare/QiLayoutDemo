3884c2090445ec18a0333bc7e1a4a95dc773b0d2
huangxianshuai <huangxianshuai@360.cn>
2019-05-20 23:32:13 +0800

    Constraint

--- QiLayoutDemo/Constraint/QiConstraintViewController.m
+++ QiLayoutDemo/Constraint/QiConstraintViewController.m
@@ -10,23 +10,24 @@
 
 @interface QiConstraintViewController ()
 
+@property (weak, nonatomic) IBOutlet UIView *contentView;
+@property (weak, nonatomic) IBOutlet UIView *subView1;
+@property (weak, nonatomic) IBOutlet UIView *subView2;
+@property (weak, nonatomic) IBOutlet UIView *subView3;
+@property (weak, nonatomic) IBOutlet UIView *subView4;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *subView1Trailing;
+
 @end
 
 @implementation QiConstraintViewController
 
 - (void)viewDidLoad {
+    
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        self.subView1Trailing.constant *= 2;
+    });
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
